d0e30bf1e0ac6186da326bb5b21a1458b1d9ee20
Michalous <49964028+Michalous@users.noreply.github.com>
2024-07-01 22:49:51 +0100

    fern added

--- script.js
+++ script.js
@@ -1,15 +1,67 @@
 document.addEventListener('DOMContentLoaded', function() {
     let triangleButton = document.getElementById('triangle')
     let carpetButton = document.getElementById('carpet')
+    let fernButton = document.getElementById('fern')
 
     var canvas = document.getElementById('myCanvas')
     var ctx = canvas.getContext('2d')
+
     var carpetCanvas = document.getElementById('carpetCanvas')
     var carpetCtx = carpetCanvas.getContext('2d')
 
+    var fernCanvas = document.getElementById('fernCanvas')
+    var fernCtx = fernCanvas.getContext('2d')
 
     triangleButton.addEventListener('click', triangle)
     carpetButton.addEventListener('click', carpet)
+    fernButton.addEventListener('click', fern)
+    
+    function fern() {
+        fernCtx.clearRect(0, 0, canvas.width, canvas.height)
+        fernButton.disabled = true
+        var x = 0.0
+        var y = 0.0
+        var plotX = []
+        var plotY = []
+
+        const SIZE = 100000
+
+        for (var i = 0; i < SIZE; i++) {
+            var r = Math.random()
+            if (r < 0.01) {
+                var xn = 0.0
+                var yn = 0.16 * y
+            }
+            else if (r < 0.86) {
+                var xn = 0.85 * x + 0.04 * y
+                var yn = -0.04 * x + 0.85 * y + 1.6
+            }
+            else if (r < 0.93) {
+                var xn = 0.2 * x - 0.26 * y
+                var yn = 0.23 * x + 0.22 * y + 1.6
+            }
+            else {
+                var xn = -0.15 * x + 0.28 * y
+                var yn = 0.26 * x + 0.24 * y + 0.44
+            }
+            plotX.push(xn)
+            plotY.push(yn)
+            x = xn
+            y = yn
+        }
+    
+        for (var i = 0; i < SIZE; i++) {
+            (function(i) {
+                setTimeout(function() {
+                    drawFernDot((2.5 + plotX[i]) * 115, 600 - plotY[i] * 60, 1)
+                    document.getElementById('fernIteration').innerHTML = i + 1
+                    if (i == SIZE - 1) {
+                        fernButton.disabled = false
+                    }
+                }, i * 0.2)
+            })(i);
+        }
+    }
     
     function triangle() {
         ctx.clearRect(0, 0, canvas.width, canvas.height)
@@ -120,6 +172,13 @@ document.addEventListener('DOMContentLoaded', function() {
         carpetCtx.arc(x, y, r, 0, 2 * Math.PI);
         carpetCtx.fillStyle = colour;
         carpetCtx.fill();
+    }
+
+    function drawFernDot(x, y, r) {
+        fernCtx.beginPath();
+        fernCtx.arc(x, y, r, 0, 2 * Math.PI);
+        fernCtx.fillStyle = 'green';
+        fernCtx.fill();
     }
 
     function drawDot(x, y, r) {
